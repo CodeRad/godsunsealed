85bf3c132caa416a1f2ab2cd92b986a4347ba850
CodeRad <CodeRad@users.noreply.github.com>
2024-01-02 01:42:47 -0800

    Adjust wreath overlay nesting

--- godsUnsealed.js
+++ godsUnsealed.js
@@ -691,34 +691,6 @@ async function displayPlayerPanel(panelId, playerInfo, playerMatchInfo) {
     });
 }
 
-// async function displayCardList(cardIds, containerId) {
-//     const cardListDiv = document.getElementById(containerId);
-
-//     // Clear previous content
-//     cardListDiv.innerHTML = '';
-
-//     // Fetch card information for all cards
-//     const cardInfoArray = await Promise.all(cardIds.map(fetchCardInfo));
-
-//     // Sort cards by mana cost
-//     cardInfoArray.sort((a, b) => a.mana - b.mana);
-
-//     // Create and append card elements
-//     for (const cardInfo of cardInfoArray) {
-//         const cardElement = document.createElement('img');
-//         cardElement.src = `https://images.godsunchained.com/art2/250/${cardInfo.id}.webp`;
-//         cardElement.title = `(${cardInfo.mana}) ${cardInfo.name}`;
-//         cardElement.className = 'card-icon';
-
-//         // Attach click event listener to each card
-//         cardElement.addEventListener('click', () => {
-//             openCardModal(cardInfo);
-//         });
-
-//         cardListDiv.appendChild(cardElement);
-//     }
-// }
-
 async function displayCardList(cardIds, containerId) {
     const cardListDiv = document.getElementById(containerId);
 
@@ -735,25 +707,25 @@ async function displayCardList(cardIds, containerId) {
     for (const cardInfo of cardInfoArray) {
         const cardElement = document.createElement('div');
         cardElement.className = 'card-container';
-
-        const cardImage = document.createElement('img');
-        cardImage.src = `https://images.godsunchained.com/art2/250/${cardInfo.id}.webp`;
+    
+        const cardImage = document.createElement('div'); // Change from img to div
+        cardImage.style.backgroundImage = `url(https://images.godsunchained.com/art2/250/${cardInfo.id}.webp)`;
         cardImage.title = `(${cardInfo.mana}) ${cardInfo.name}`;
         cardImage.className = 'card-icon';
-
+    
         // Check if the card is legendary
         if (cardInfo.rarity === 'legendary') {
-            const legendaryOverlay = document.createElement('img');
-            legendaryOverlay.src = 'images/wreath.png';
+            const legendaryOverlay = document.createElement('div'); // Change from img to div
+            legendaryOverlay.style.backgroundImage = 'url(images/wreath.png)';
             legendaryOverlay.className = 'legendary-overlay';
-            cardElement.appendChild(legendaryOverlay);
+            cardImage.appendChild(legendaryOverlay); // Append the legendaryOverlay to cardImage
         }
-
+    
         // Attach click event listener to each card
         cardImage.addEventListener('click', () => {
             openCardModal(cardInfo);
         });
-
+    
         cardElement.appendChild(cardImage);
         cardListDiv.appendChild(cardElement);
     }
